a1189ae841a8c6843fa5eca9edbc0556156cb7d2
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2022-04-02 14:30:12 +0200

    RC4

--- src/main/scala/zio/pravega/PravegaTable.scala
+++ src/main/scala/zio/pravega/PravegaTable.scala
@@ -2,6 +2,8 @@ package zio.pravega
 
 import zio._
 import zio.stream._
+import io.pravega.client.KeyValueTableFactory
+import io.pravega.client.tables.KeyValueTableClientConfiguration
 
 trait PravegaTableService {
   def sink[K, V](
@@ -15,4 +17,24 @@ trait PravegaTableService {
   ): Task[ZStream[Any, Throwable, (K, V)]]
 }
 
-final case class PravegaTable()
+final case class PravegaTable(
+    keyValueTableFactory: KeyValueTableFactory,
+    kvtClientConfig: KeyValueTableClientConfiguration
+) extends PravegaTableService {
+
+  override def sink[K, V](
+      tableName: String,
+      settings: TableWriterSettings[K, V]
+  ): RIO[Scope, ZSink[Any, Throwable, (K, V), Nothing, Unit]] = {
+    val table =
+      keyValueTableFactory.forKeyValueTable(tableName, kvtClientConfig)
+    println(table)
+    ???
+  }
+
+  override def source[K, V](
+      readerGroupName: String,
+      settings: TableReaderSettings[K, V]
+  ): Task[ZStream[Any, Throwable, (K, V)]] = ???
+
+}
