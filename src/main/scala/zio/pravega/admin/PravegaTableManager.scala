55c3046500236b8e33e1cea222151402d7355798
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2022-11-11 18:30:37 +0100

    More layers (#80)
    * :recycle: User layes with KVP tables
    
    * Use layers to share kvp table maanger

--- src/main/scala/zio/pravega/admin/PravegaTableManager.scala
+++ src/main/scala/zio/pravega/admin/PravegaTableManager.scala
@@ -18,20 +18,15 @@ trait PravegaTableManager {
 
 }
 
-private case class PravegaTableManagerImpl(clientConfig: ClientConfig) extends PravegaTableManager {
-
-  def keyValueTableManager(): RIO[Scope, KeyValueTableManager] =
-    ZIO.attemptBlocking(KeyValueTableManager.create(clientConfig)).withFinalizerAuto
+private case class PravegaTableManagerImpl(keyValueTableManager: KeyValueTableManager) extends PravegaTableManager {
 
   override def createTable(scope: String, tableName: String, config: KeyValueTableConfiguration): RIO[Scope, Boolean] =
     for {
-      keyValueTableManager <- keyValueTableManager()
-      created              <- ZIO.attemptBlocking(keyValueTableManager.createKeyValueTable(scope, tableName, config))
+      created <- ZIO.attemptBlocking(keyValueTableManager.createKeyValueTable(scope, tableName, config))
     } yield created
 
   override def dropTable(scope: String, tableName: String): RIO[Scope, Boolean] = for {
-    keyValueTableManager <- keyValueTableManager()
-    deleted              <- ZIO.attemptBlocking(keyValueTableManager.deleteKeyValueTable(scope, tableName))
+    deleted <- ZIO.attemptBlocking(keyValueTableManager.deleteKeyValueTable(scope, tableName))
   } yield deleted
 
 }
@@ -48,12 +43,19 @@ object PravegaTableManager {
   def dropTable(scope: String, tableName: String): RIO[PravegaTableManager & Scope, Boolean] = ZIO
     .serviceWithZIO[PravegaTableManager](_.dropTable(scope, tableName))
 
+  private def keyValueTableManager(clientConfig: ClientConfig) = ZIO
+    .attemptBlocking(KeyValueTableManager.create(clientConfig))
+    .withFinalizerAuto
+    .map(keyValueTableManager => PravegaTableManagerImpl(keyValueTableManager))
+
   def live(
     clientConfig: ClientConfig
-  ): ZLayer[Any, Nothing, PravegaTableManager] =
-    ZLayer.succeed(new PravegaTableManagerImpl(clientConfig))
+  ): RLayer[Scope, PravegaTableManager] =
+    ZLayer.fromZIO(keyValueTableManager(clientConfig))
 
-  val live: ZLayer[ClientConfig, Nothing, PravegaTableManager] =
-    ZLayer.fromFunction(PravegaTableManagerImpl(_))
+  val live: RLayer[Scope & ClientConfig, PravegaTableManager] =
+    ZLayer.fromZIO(
+      ZIO.serviceWithZIO[ClientConfig](keyValueTableManager)
+    )
 
 }
