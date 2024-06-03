9d2dd533723d9f364f41e9a6f9233faf1b62a388
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-06-03 16:13:22 +0200

    Test Sonarqube activation

--- modules/zio-pravega/src/main/scala/zio/Accessible.scala
+++ modules/zio-pravega/src/main/scala/zio/Accessible.scala
@@ -1,5 +1,5 @@
 package zio
 
 import scala.annotation.StaticAnnotation
-// This annoation is used by Scalafix
+// This annoation is used by Scalafix to rewrite the code
 class Accessible extends StaticAnnotation
